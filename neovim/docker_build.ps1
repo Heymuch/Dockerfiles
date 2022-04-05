33fa2273749c6156e51837fd8ff4309cdbb982d3
Jiří Hejduk <jiri@hejduk.in>
2022-04-05 10:24:31 +0200

    neovim on alpine

---
+++ neovim/docker_build.ps1
@@ -0,0 +1,1 @@
+docker build . -t heymuch/neovim -f .\Dockerfile.alpine
---
+++ neovim/docker_run.ps1
@@ -0,0 +1,2 @@
+# docker run --rm -it -v (Get-Location)/config:/home/user/.config/nvim heymuch/neovim
+docker run --rm -it -v ${PWD}:/workspace heymuch/neovim
